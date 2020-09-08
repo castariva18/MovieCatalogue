2c9de1ce0753cfc83553f16c65aed52741485b23
Belmiro <tengku13elmiro@gmail.com>
2020-09-08 20:46:33 +0700

    detail

--- app/src/main/java/com/suncode/moviecatalogue/adapter/MovieAdapter.java
+++ app/src/main/java/com/suncode/moviecatalogue/adapter/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.suncode.moviecatalogue.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +13,12 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.suncode.moviecatalogue.Constant;
 import com.suncode.moviecatalogue.R;
 import com.suncode.moviecatalogue.model.Movie;
+import com.suncode.moviecatalogue.view.DetailActivity;
 
 import java.util.List;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
@@ -45,6 +48,22 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         String image = mData.get(position).getBackdrop_path();
         Glide.with(context).load(Constant.IMAGE_REQUEST + image).diskCacheStrategy(DiskCacheStrategy.DATA).into(holder.img);
 
+        String poster = mData.get(position).getBackdrop_path();
+        String title = mData.get(position).getTitle();
+        String overview = mData.get(position).getOverview();
+        //int[] genreId = mData.get(position).getGenre_ids();
+
+        holder.cdMovie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, DetailActivity.class);
+                intent.putExtra("TITLE",title);
+                intent.putExtra("POSTER",poster);
+                intent.putExtra("OVERVIEW",overview);
+          //      intent.putExtra("GENRE", genreId);
+                context.startActivity(intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+            }
+        });
     }
 
     @Override
@@ -55,12 +74,14 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     public class ViewHolder extends RecyclerView.ViewHolder {
         private TextView title, overview;
         private ImageView img;
+        private ConstraintLayout cdMovie;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.tv_title);
             overview = itemView.findViewById(R.id.tv_desc);
             img = itemView.findViewById(R.id.img_poster);
+            cdMovie = itemView.findViewById(R.id.card1);
         }
     }
 }
--- app/src/main/java/com/suncode/moviecatalogue/view/DetailActivity.java
+++ app/src/main/java/com/suncode/moviecatalogue/view/DetailActivity.java
@@ -1,16 +1,84 @@
 package com.suncode.moviecatalogue.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.suncode.moviecatalogue.Constant;
 import com.suncode.moviecatalogue.R;
+import com.suncode.moviecatalogue.api.ApiClient;
+import com.suncode.moviecatalogue.api.ApiService;
+import com.suncode.moviecatalogue.model.Genre;
+
+import java.util.HashMap;
 
 public class DetailActivity extends AppCompatActivity {
+    private ImageView imgPoster;
+    private TextView tvOverview, tvTitle, tvGenre;
+    private ApiService apiService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        imgPoster = findViewById(R.id.img_detail);
+        tvTitle = findViewById(R.id.title_detail);
+        tvOverview = findViewById(R.id.desc_detail);
+        tvGenre = findViewById(R.id.genre_detail);
+
+        apiService = ApiClient.getRetrofit().create(ApiService.class);
+
+        Intent intent = getIntent();
+        String movie_title = intent.getStringExtra("TITLE");
+//        String movie_title_ori = intent.getStringExtra("ORI_TITLE");
+        String movie_poster = intent.getStringExtra("POSTER");
+//        String movie_release = intent.getStringExtra("RELEASE");
+        String movie_overview = intent.getStringExtra("OVERVIEW");
+//        int movie_vote_count = intent.getIntExtra("VOTE_COUNT",0);
+//        int movie_vote_avg = intent.getIntExtra("VOTE_AVG",0);
+//        double popular = intent.getDoubleExtra("POPULAR",0);
+        int[] genre_ids = intent.getIntArrayExtra("GENRE");
+        Glide.with(this).load(Constant.IMAGE_REQUEST + movie_poster).diskCacheStrategy(DiskCacheStrategy.DATA).into(imgPoster);
+        //  getGenre(genre_ids);
+        tvOverview.setText(movie_overview);
+        tvTitle.setText("Title: " + movie_title);
     }
+
+//    private void getGenre(int[] genre_ids) {
+//        Call<Genre> genreCall = apiService.getGenres(Constant.API_KEY, Constant.LANGUAGE);
+//        genreCall.enqueue(new Callback<Genre>() {
+//            @Override
+//            public void onResponse(Call<Genre> call, Response<Genre> response) {
+//                Genre data = response.body();
+//
+//                Genre.Genres[] genres = data.getGenres();
+//
+//                HashMap<Integer, String> map = new HashMap<>();
+//
+//                for (int i = 0; i < genres.length; i++) {
+//                    map.put(genres[i].getId(), genres[i].getName());
+//                }
+//
+//                for (int i = 0; i < genre_ids.length; i++) {
+//                    String genreName = map.get(genre_ids[i]);
+//                    tvGenre.setText("Genre: " + genreName);
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onFailure(Call<Genre> call, Throwable t) {
+//                t.getMessage();
+//            }
+//        });
+//    }
 }
